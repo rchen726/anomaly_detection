8e386ff90f53577ad5094c9b5e4c3fc2515e98ad
rchen726 <rchen726@gmail.com>
2017-07-05 07:41:35 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,47 @@
+Synopsis
+
+This code builds a real-time platform to analyze purchases within a social network of users, and detect any behavior that is far from the average within that social network.
+
+Code Requirements
+
+Please include two input files. One json file has past data used to build the initial state of the entire user network, as well as the purchase history of the users. Another one will be used to determine whether a purchase is anomalous. 
+
+Motivation
+
+Users can see which items their friends are buying, and are influenced by the purchases within their network.
+
+Installation and Running
+
+No installation needed. Just use run.sh file. Please change to the corresponding location of three json files. First two json files are input files and last one is the output file.
+
+Tests
+
+One available under the insight_testsuite folder. 
+
+Contributors
+
+For any question, please contact Ru Chen with email rchen726@gmail.com.
+
+License
+
+MIT License
+
+Copyright (c) 2017 rchen726
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ src/anomaly.py
@@ -0,0 +1,126 @@
+import codecs
+import json
+import os
+import numpy as np
+import time
+from collections import defaultdict
+from collections import deque
+from operator import itemgetter
+import sys
+
+def AddFriend(id1,id2,friend_list):
+    friend_list[id1].add(id2)
+    friend_list[id2].add(id1)
+
+def UnFriend(id1,id2,friend_list):
+    friend_list[id1].remove(id2)
+    friend_list[id2].remove(id1)
+
+def Purchase_update(person_id,  amount, purchase_tot, purchase_count, T):
+    purchase_count += 1
+    if person_id not in purchase_tot:
+        purchase_tot[person_id] =  deque([], T)
+    purchase_tot[person_id].append((purchase_count, amount))
+    return purchase_count
+    
+def Find_upto_DFriend(person_id, friend_list, D):
+    visited_list = set()
+    work_list = [(person_id, 0)]
+    idx = 0
+    while idx < len(work_list):
+        current = work_list[idx]
+        idx += 1
+        if not current[0] in visited_list and current[1] < D:
+            for friend in friend_list[current[0]]:
+                work_list.append((friend, current[1] + 1))
+        visited_list.add(current[0])
+    visited_list.remove(person_id)
+    return visited_list
+
+def DFriend_purchases(DFriends, T, purchase_tot):
+    purchase=[]
+    for friend_id in DFriends:
+        if friend_id in purchase_tot:
+            purchase.extend(purchase_tot[friend_id])
+    tmp = sorted(purchase, key=lambda x: x[0])
+    tmp2 = tmp[-T::]
+    #recent_purchases = [x[1] for x in tmp2]
+    recent_purchases = list(map(itemgetter(1), tmp2))
+    purchases_num = len(recent_purchases)
+    network_mean = np.mean(recent_purchases)
+    network_std = np.std(recent_purchases)
+    return purchases_num, network_mean, network_std
+
+def test_anomaly(amount, purchases_num, network_mean, network_std):
+    if purchases_num >= 2 and amount >= network_mean + 3 * network_std:
+        return True
+    return False
+
+def jsonWrite_anomaly(file_path, line, mean, std):
+    line.update({'mean':  '{0:.2f}'.format(mean), 'sd':  '{0:.2f}'.format(std)})
+    with open(file_path, 'a') as f:
+        json.dump(line, f)
+        f.write('\n')
+
+def run_batch(path_batch):
+    purchase_count = 0
+    friend_list = defaultdict(set)
+    purchase_tot = {}    
+    
+    with codecs.open(path_batch,'rU','utf-8') as f:
+        for num, line in enumerate(f):
+            each = json.loads(line)
+            if num == 0:
+                if 'D' not in each and 'T' not in each:
+                    print('I can not find D and T in the batch log!!!')
+                    print('returning default values D=2 and T=50')
+                    D=2
+                    T=50
+                else:
+                    D = int(each['D'])
+                    T = int(each['T'])
+            else:
+                if each['event_type']=='befriend':
+                    AddFriend(each['id1'], each['id2'], friend_list)
+                if each['event_type']=='unfriend':
+                    UnFriend(each['id1'], each['id2'], friend_list)
+                if each['event_type']=='purchase':
+                    purchase_count = Purchase_update(each['id'], float(each['amount']), purchase_tot , purchase_count, T)
+    return D, T, purchase_count, friend_list, purchase_tot
+
+    
+def run_stream(path_stream, path_flagged,  D, T, purchase_count, friend_list, purchase_tot):
+    open(path_flagged, 'w').close()
+        
+    with codecs.open(path_stream,'rU','utf-8') as f:
+        for line in f:
+            if line == '\n':
+                break
+            else:
+                each = json.loads(line)           
+                if each['event_type']=='purchase':
+                    # Calculate the network of this specific ID up to D degree
+                    DFriend_list_union = Find_upto_DFriend(each['id'], friend_list, D)
+                    # find the recent purchase between [2, T] in this ID's network
+                    purchases_num, network_mean, network_std = DFriend_purchases(DFriend_list_union, T, purchase_tot)
+                    # find the mean and std of the recent purchase, determine this specific purchase is anomalous
+                    if test_anomaly(float(each['amount']),purchases_num, network_mean, network_std) == True:
+                        jsonWrite_anomaly(path_flagged, each, network_mean, network_std)               
+                    purchase_count = Purchase_update(each['id'],  float(each['amount']), purchase_tot , purchase_count, T)        
+                if each['event_type']=='befriend':
+                    AddFriend(each['id1'], each['id2'], friend_list)
+                if each['event_type']=='unfriend':
+                    UnFriend(each['id1'], each['id2'], friend_list)
+    
+
+
+def main(path_batch, path_stream, path_flagged):
+
+    D, T, purchase_count, friend_list, purchase_tot = run_batch(path_batch)
+    run_stream(path_stream, path_flagged, D, T, purchase_count, friend_list, purchase_tot)
+    
+if __name__ == "__main__":
+    t = time.time()
+    main(sys.argv[1], sys.argv[2], sys.argv[3])            
+    elapsed = time.time() - t
+    print('time', elapsed)            
